5da0f3b1acce7e64a0f7e33d4a97d20d40fbe765
Eve162513 <1011865068@qq.com>
2018-05-06 20:18:09 +0800

    基于回调的事件处理机制

---
+++ app/src/main/java/cn/edu/hqu/cst/androidproject3/CallbackHandler.java
@@ -0,0 +1,16 @@
+package cn.edu.hqu.cst.androidproject3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2018/5/6.
+ */
+
+public class CallbackHandler extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
